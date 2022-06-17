e7961b9ba82059f9cf84734af7132a6412c4f618
SAID ELKANTAOUI <44905408+thehappyman@users.noreply.github.com>
2022-06-17 22:17:48 +0100

    Added a navbar component with a Home link. (#4)
    * Added a navbar component with a Home link.
    
    * Made the requested changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
 }
 
 export default App;
