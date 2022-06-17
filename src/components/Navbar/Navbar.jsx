71af18b8e607d45d713567b13904ed3d1cff6ab2
Soufiane <62034446+gitsoufiane@users.noreply.github.com>
2022-06-17 14:32:16 -0700

    set prettier rules and add husky to format on push

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <div className="bg-black text-white font-bold py-2 px-10 flex flex-row">
-                <h1 className="basis-3/4 text-2xl">Movies App</h1>
-                <Link to='/' className="basis-1/12">Home</Link>
-                <h2 className="basis-1/12">Movies</h2>
-                <h2 className="basis-1/12">Series</h2>
-        </div>
-    );
+  return (
+    <div className="bg-black text-white font-bold py-2 px-10 flex flex-row">
+      <h1 className="basis-3/4 text-2xl">Movies App</h1>
+      <Link to="/" className="basis-1/12">
+        Home
+      </Link>
+      <h2 className="basis-1/12">Movies</h2>
+      <h2 className="basis-1/12">Series</h2>
+    </div>
+  );
 };
 
 export default Navbar;
